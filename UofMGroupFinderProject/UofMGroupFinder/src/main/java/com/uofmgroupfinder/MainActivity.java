d508719b76bdd59495d0564db0d6ae11ee680810
vikindragoon <strom441@umn.edu>
2013-11-20 09:41:26 -0600

    TA Meeting Update
    This is the project I showed to the TA. There is multiple screens that
    can be worked on now and I have some kind of page control added in.
    Please DO NOT use or expand to the current way the pages are navigated
    between. This should use an event listener instead of the shitty way I
    used. Either please change this or leave it for me

--- UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/MainActivity.java
+++ UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/MainActivity.java
@@ -1,15 +1,15 @@
 package com.uofmgroupfinder;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -23,14 +23,45 @@ public class MainActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        Button eventBtn = (Button) findViewById(R.id.eventSearchButton);
+        //eventBtn.setOnClickListener(new View.OnClickListener() {
+            //public void onClick(View v){
+                //Intent i=new Intent( MainActivity.this, EventActivity.class);
+                //startActivity(i);
+                //setResult(RESULT_OK, i);
+                //finish();
+                //Intent myIntent = new Intent(v.getContext(), EventActivity.class);
+                //startActivityForResult(myIntent, 0);
+            //}
+        //});
+
     }
 
+    public void gotoGroup(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, GroupActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoEvent(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, EventActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoManagement(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, ManagementActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.title_bar, menu);
         return true;
     }
 
@@ -50,14 +81,38 @@ public class MainActivity extends Activity {
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        //Button btn;
 
         public PlaceholderFragment() {
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+                return rootView;
+        }
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class EventFragment extends Fragment {
+
+        public EventFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_event, container, false);
             return rootView;
         }
     }

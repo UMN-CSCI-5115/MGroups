654147916f0b561e4fc357cf01ee0bbeba3d9193
vikindragoon <strom441@umn.edu>
2013-12-03 20:15:33 -0600

    added in work on search and action bar

--- UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/EventActivity.java
+++ UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/EventActivity.java
@@ -3,20 +3,31 @@ package com.uofmgroupfinder;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MenuInflater;
+import android.widget.Toast;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.os.Build;
 
-public class EventActivity extends Activity {
+public class EventActivity extends Activity implements OnQueryTextListener {
+    TextView mSearchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSearchText = new TextView(this);
+        mSearchText.setPadding(10, 10, 10, 10);
+        mSearchText.setText("Action Bar Usage example from CoderzHeaven");
         setContentView(R.layout.activity_event);
+        //setContentView(mSearchText);
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
@@ -31,6 +42,8 @@ public class EventActivity extends Activity {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.event, menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_settings).getActionView();
+        searchView.setOnQueryTextListener(this);
         return true;
     }
 
@@ -40,6 +53,8 @@ public class EventActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+
         if (id == R.id.action_settings) {
             return true;
         }
@@ -61,5 +76,29 @@ public class EventActivity extends Activity {
             return rootView;
         }
     }
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    
+ // The following callbacks are called for the SearchView.OnQueryChangeListener
+    public boolean onQueryTextChange(String newText) {
+        newText = newText.isEmpty() ? "" : "Query so far: " + newText;
+        mSearchText.setText(newText);
+        mSearchText.setTextColor(Color.GREEN);
+        return true;
+    }
+ 
+    public boolean onQueryTextSubmit (String query) {
+        Toast.makeText(this, "Searching for: " + query + "...", Toast.LENGTH_SHORT).show();
+        mSearchText.setText("Searching for: " + query + "...");
+        mSearchText.setTextColor(Color.RED);
+        return true;
+    }
+    
+    
 
 }
--- UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/MainActivity.java
+++ UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends Activity {
         
         // Inflate the menu; this adds items to the action bar if it is present.
         //getMenuInflater().inflate(R.menu.main, menu);
-        getMenuInflater().inflate(R.menu.title_bar, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 

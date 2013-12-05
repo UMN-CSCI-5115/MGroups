6a7af680318661108e090bf4e8b6f5290db2dfc5
sivag001 <sivag001@umn.edu>
2013-12-05 16:25:32 -0600

    some more changes. fixing naming conflicts.

--- UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/ResultsActivity.java
+++ UofMGroupFinderProject/UofMGroupFinder/src/main/java/com/uofmgroupfinder/ResultsActivity.java
@@ -63,7 +63,7 @@ public class ResultsActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_resultsgroup, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_result, container, false);
             return rootView;
         }
     }
